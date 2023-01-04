9b21c11667a0ecc6b6fd8c0324b8a7de48631532
riverback <hhz_pku@163.com>
2023-01-04 19:44:35 +0800

    绘制混淆矩阵

---
+++ confusion_matrix.py
@@ -0,0 +1,62 @@
+# confusion_matrix
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+
+classes = ['video', 'streaming', 'browsing']
+confusion_matrix = np.array(
+    [[4, 1, 1],
+     [1, 14, 3],
+     [0, 0, 4]
+     ], dtype=np.int)  # 输入特征矩阵
+proportion = []
+length = len(confusion_matrix)
+print(length)
+for i in confusion_matrix:
+    for j in i:
+        temp = j / (np.sum(i))
+        proportion.append(temp)
+# print(np.sum(confusion_matrix[0]))
+# print(proportion)
+pshow = []
+for i in proportion:
+    pt = "%.2f%%" % (i * 100)
+    pshow.append(pt)
+proportion = np.array(proportion).reshape(length, length)  # reshape(列的长度，行的长度)
+pshow = np.array(pshow).reshape(length, length)
+# print(pshow)
+config = {
+    "font.family": 'Times New Roman',  # 设置字体类型
+}
+rcParams.update(config)
+plt.imshow(proportion, interpolation='nearest', cmap=plt.cm.Blues)  # 按照像素显示出矩阵
+# (改变颜色：'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds','YlOrBr', 'YlOrRd',
+# 'OrRd', 'PuRd', 'RdPu', 'BuPu','GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn')
+# plt.title('confusion_matrix')
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, fontsize=12)
+plt.yticks(tick_marks, classes, fontsize=12)
+
+thresh = confusion_matrix.max() / 2.
+# iters = [[i,j] for i in range(len(classes)) for j in range((classes))]
+
+iters = np.reshape([[[i, j] for j in range(length)] for i in range(length)], (confusion_matrix.size, 2))
+for i, j in iters:
+    if (i == j):
+        plt.text(j, i - 0.12, format(confusion_matrix[i, j]), va='center', ha='center', fontsize=10, color='white',
+                 weight=5)  # 显示对应的数字
+        plt.text(j, i + 0.12, pshow[i, j], va='center', ha='center', fontsize=10, color='white')
+    else:
+        plt.text(j, i - 0.12, format(confusion_matrix[i, j]), va='center', ha='center', fontsize=10)  # 显示对应的数字
+        plt.text(j, i + 0.12, pshow[i, j], va='center', ha='center', fontsize=10)
+
+plt.ylabel('True label', fontsize=16)
+plt.xlabel('Predict label', fontsize=16)
+plt.tight_layout()
+plt.show()
+plt.savefig('已知A未知B.png')
+
+
